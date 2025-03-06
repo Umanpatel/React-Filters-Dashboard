2fa242879a48dc09177408cd737c34d4cee57adc
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-03-06 03:49:43 -0500

    Create Navbar

--- src/Navigation/nav.jsx
+++ src/Navigation/nav.jsx
@@ -1,10 +1,29 @@
-import "./nav.css"
+import "./nav.css";
+import { FiHeart } from "react-icons/fi";
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai';
 
 const nav = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <nav>
+      <div className="nav-container">
+        <input 
+          type="text" 
+          className="search-input" 
+          placeholder="Enter your search shoes"
+        />
+        <div className="profile-container">
+          <a href="/">
+            <FiHeart  className="nav-icons"/>
+          </a>
+          <a href="/">
+            <AiOutlineShoppingCart className="nav-icons"/>
+          </a>
+          <a href="/">
+            <AiOutlineUserAdd className="nav-icons"/>
+          </a>
+        </div>
+      </div>
+    </nav>
   )
 }
 
